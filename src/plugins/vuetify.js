28da2aabc318b48dbef28c9ebc7b603ee1bfb9a1
0101dd <aa09335665@gmail.com>
2022-03-02 19:11:04 +0800

    font

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib/framework'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify)
 
 export default new Vuetify({
   icons: {
-    iconfont: 'mdiSvg' // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+    iconfont: 'mdi' // 默认值 - 仅用于展示目的
   },
   theme: {
     dark: false,
